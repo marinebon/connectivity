c753d71f6147a33d57b35332dd264cb26ab0df35
Stephanie Gad <sgad@ESM.UCSB.EDU>
2017-07-20 15:04:38 -0700

    + agg_rasters()

--- prep_rasters.R
+++ prep_rasters.R
@@ -1,3 +1,4 @@
+# load packages
 library(sp)
 library(rgdal)
 library(raster)
@@ -7,29 +8,29 @@ library(knitr)
 library(mapview)
 library(marmap) # devtools::install_github('ericpante/marmap')
 
+# setup working directory
 dir_data = switch(
   R.utils::System$getUsername(),
   bbest = '/Volumes/Best HD/mbon_data_big',
   sgad  = 'P:/habitats')
 
+# setup dirs
 dir_hab = dir_data
 dir_tmp = file.path(dir_hab, 'tmp')
 dir.create(dir_tmp, showWarnings=F)
 dir.create(dir_hab, showWarnings=F)
 
-
-tif_all  = file.path(dir_data, 'hab_id.tif')
-csv_all  = file.path(dir_data, 'hab_id.csv')
-csv_sources = file.path('hab_sources.csv')
-
 # paths & vars
-tif_m_g  = 'P:/habitats/ocean_mask.tif'
-gdb      = 'H:/MBON/study_area.gdb'
-regions  = c('southeast', 'southwest', 'channelislands')
+tif_all     = file.path(dir_data, 'hab_id.tif')
+csv_all     = file.path(dir_data, 'hab_id.csv')
+csv_sources = file.path('hab_sources.csv')
+tif_m_g     = file.path(dir_data, 'ocean_mask.tif')
+gdb         = 'H:/MBON/study_area.gdb'
+regions     = c('southeast', 'southwest', 'channelislands')
+redo        = FALSE
 
 # read global water mask raster
 r_m_g = raster(tif_m_g)
-fact_100km = 100000 %/% xres(r_m_g)
 
 for (rgn in regions){ # rgn = 'channelislands'
   
@@ -37,47 +38,73 @@ for (rgn in regions){ # rgn = 'channelislands'
   tif_m     = sprintf('P:/habitats/%s-1km_mask.tif'   , rgn)
   tif_p     = sprintf('P:/habitats/%s-1km_patchid.tif', rgn)
   tif_c     = sprintf('P:/habitats/%s-1km_percentcover.tif', rgn)
-  tif_m_100km = sprintf('P:/habitats/%s-100km_mask.tif'   , rgn)
-  tif_p_100km = sprintf('P:/habitats/%s-100km_patchid.tif', rgn)
-  tif_c_100km = sprintf('P:/habitats/%s-100km_percentcover.tif', rgn)
+  
+  # water mask: logical (16-bit signed integer)
+  if (!file.exists(tif_m)){
+    
+    # read in buffer in same projection as global water mask (Mollweide)
+    buf = readOGR(gdb, sprintf('%s_buf_mol', rgn))
+    #buf = readOGR(gdb, 'channelislands') # TODO: resolve difference w/ other regions
+    
+    # generate Water mask raster & patch cover raster
+    r_m = crop(r_m_g, buf) %>% # water = 1; land = 0
+      trim() %>%
+      mask(buf)
+    r_m[r_m==0] = NA # convert land to NA for single mask value
+    as.logical(r_m, filename=tif_m, overwrite=T)
+  } else {
+    r_m = raster(tif_m)
+  }
+  
+  # patchid: integer (32-bit signed integer)
+  if (!file.exists(tif_p)){
 
-  # read in buffer in same projection as global water mask (Mollweide)
-  buf = readOGR(gdb, sprintf('%s_buf_mol', rgn))
-  #buf = readOGR(gdb, 'channelislands') # TODO: resolve difference w/ other regions
+    # generate patch IDs raster
+    r_p = r_m
+    r_p[!is.na(r_p)] = 1:sum(!is.na(getValues(r_p))) # plot(r_p)
+    as.integer(r_p, filename=tif_p, overwrite=T)
+    
+  } else {
+    r_m = raster(tif_p)
+  }
+  
+  # percentcover: floating (64-bit double precision)
+  if (!file.exists(tif_c)){
+    
+    # generate percent cover raster
+    r_c = r_m
+    r_c[r_c==1] = 1.0
+    writeRaster(r_c, tif_c, overwrite=T)
+  }
+}
 
-  # generate Water mask raster & patch cover raster
-  r_m = crop(r_m_g, buf) %>% # water = 1; land = 0
-    trim() %>%
-    mask(buf)
-  r_m[r_m==0] = NA # convert land to NA for single mask value
+agg_rasters = function(tif_m_1km, km_factor=100){
+  # tif_m should end with: "-1km_mask.tif"
+  #
+  # tif_m_in = 'P:/habitats/channelislands_700buf-1km_mask.tif'; km_factor=100
+  # tif_m_in = 'P:/habitats/channelislands-1km_mask.tif'; km_factor=100
+
+  pfx = str_replace(tif_m, '(.*)-1km_mask.tif', '\\1')
+  tif_m = sprintf('%s-%dkm_mask.tif'        , pfx, km_factor)
+  tif_p = sprintf('%s-%dkm_patchid.tif'     , pfx, km_factor)
+  tif_c = sprintf('%s-%dkm_percentcover.tif', pfx, km_factor)
+
+  # reduce resolution, ie increase pixel size to 9 km
+  r_m_1km = raster(tif_m_1km)
+  km_factor_int = km_factor*1000 %/% xres(r_m_1km)
+  r_m = aggregate(r_m_1km, km_factor_int)
   as.logical(r_m, filename=tif_m, overwrite=T)
 
   # generate patch IDs raster
-  #r_p = setValues(r_m, 1:ncell(r_m)) %>%
-  #  mask(r_m)
   r_p = r_m
-  n_1 = sum(!is.na(getValues(r_p)))
-  r_p[!is.na(r_p)] = 1:n_1 # plot(r_p)
-  
-  setValues(r_m, 1:ncell(r_m)) %>%
-    mask(r_m)
+  r_p[!is.na(r_p)] = 1:sum(!is.na(getValues(r_p))) # plot(r_p)
   as.integer(r_p, filename=tif_p, overwrite=T)
-  
+
   # generate percent cover raster
   r_c = r_m
   r_c[r_c==1] = 1.0
   writeRaster(r_c, tif_c, overwrite=T)
   
-  # reduce resolution, ie increase pixel size to 9 km
-  r_m_100km = aggregate(r_m, fact_100km)
-  as.logical(r_m_100km, filename=tif_m_100km, overwrite=T)
-  
-  # generate patch IDs raster
-  r_p_100km = setValues(r_m_100km, 1:ncell(r_m_100km)) %>%
-    mask(r_m_100km)
-  as.integer(r_p_100km, filename=tif_p_100km, overwrite=T)
+}
 
-  # generate percent cover raster
-  r_c_100km = r_m_100km
-  r_c_100km[r_c_100km==1] = 1.0
-  writeRaster(r_c_100km, tif_c_100km, overwrite=T)
+#agg_rasters('P:/habitats/channelislands-1km_mask.tif', km_factor=100)
